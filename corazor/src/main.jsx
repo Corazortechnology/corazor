4f8b41cd080d76a4157a5f8653b7ecd8f63010ce
Anant Jain <ritujainanant2810@gmail.com>
2024-05-06 19:50:39 +0530

    main 3

--- corazor/src/main.jsx
+++ corazor/src/main.jsx
@@ -36,11 +36,11 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
