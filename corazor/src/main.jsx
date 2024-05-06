5393e4341b796f54574544250a9eaf3fcbf88262
Anant Jain <ritujainanant2810@gmail.com>
2024-05-06 19:49:25 +0530

    main 2

--- corazor/src/main.jsx
+++ corazor/src/main.jsx
@@ -13,6 +13,23 @@
   
 // )
 
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import { BrowserRouter } from 'react-router-dom';
+// import './index.css';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -27,4 +44,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
